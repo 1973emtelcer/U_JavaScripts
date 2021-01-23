21c0b95de12a11505d78a87e552b5b5880eade53
1973emtelcer <elenacerezal@gmail.com>
2021-01-23 19:03:27 +0000

    ejercicio1

---
+++ ejercicio1_1/index.js
@@ -0,0 +1,11 @@
+// base 16 * 23 cm
+//altura 13 cm
+
+const volumen = 16*23*13;
+console.log ("volumen", volumen + "cm3");
+
+// A= al + 2ab
+const areaLateral = 23*13;
+const areaBase = 23*16;
+const areaTotal = areaLateral + 2*areaBase;
+console.log ("areaTotal", areaTotal + "cm2");
