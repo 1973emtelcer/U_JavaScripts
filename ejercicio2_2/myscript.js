4d7dc1a8c1a89b70cd58d867b138719819c015c9
1973emtelcer <elenacerezal@gmail.com>
2021-01-24 17:50:45 +0000

    ejercicio

---
+++ ejercicio2_2/myscript.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const Elena ={
+        dia: 5,
+        mes: 'Septiembre'
+};
+console.log ('Mi cumpleaños es el '  +  Elena.dia + ' de ' + Elena.mes);
+
+const meses = ['Enero', 'Febrero','Marzo', 'Abril', 'Mayo', 'Junio','Julio','Agosto', 'Septiembre', 'Octubre', 'Noviembre','Diciembre'];
+console.log (meses[9]);
