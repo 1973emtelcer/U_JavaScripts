3ac4b94be3d949ca069ef68a38bf7a8193f7da47
1973emtelcer <elenacerezal@gmail.com>
2021-01-24 18:39:11 +0000

    cambios

--- ejercicio2_3/script.js
+++ ejercicio2_3/script.js
@@ -1,5 +1,5 @@
 'use strict'
-const Datos = ["hola", 2, 5, "adios"]
+const Datos = ["hola", 2, 5, "adios"];
 //determina cual de los dos números es mayor
 console.log('5 es mayor que 2', Datos[2]>Datos[1]);
 //determina el resultado de las cinco operaciones numéricas realizadas con los dos elemento numéricos
@@ -8,3 +8,4 @@ console.log('resta', Datos[1]-Datos[2]);
 console.log ('multiplicación',  Datos[1]*Datos[2]);
 console.log ('división', Datos[1]/Datos[2]);
 console.log ('cociente',  Datos[1]%Datos[2]);
+console.log ('cociente', Datos[2]%Datos[1]);
